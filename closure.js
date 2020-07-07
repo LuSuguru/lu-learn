928c72799f3a2d7f586342b97dacd61e3a43d512
lujie <jie.lu3776@hipac.cn>
2020-07-07 10:45:19 +0800

    feat: 分类函数

--- closure.js
+++ closure.js
@@ -3,5 +3,78 @@
  * @Author: 芦杰
  * @Description: 研究闭包专用
  * @LastEditors: 芦杰
- * @LastEditTime: 2020-07-07 10:10:13
- */ 
+ * @LastEditTime: 2020-07-07 10:28:04
+ */
+
+/**
+* 研究闭包专用1
+*/
+// var a = 1;
+// function f() {
+//   var a = 2;
+//   c = 4;
+//   return function g() {
+//     console.log(a++); // 2
+//     console.log(b++); // 3
+//     console.log(c++); // 4
+//   }
+// }
+
+// console.log(a); // 1
+// console.log(b); // undefined
+// // console.log(c); // 报错
+
+// var g = f();
+// var b = 3;
+// g();
+
+// console.log(a); // 1
+// console.log(b); // 4
+// console.log(c); // 5
+
+/**
+ * 研究闭包专用2
+ */
+
+var a = 1
+var test = [{ a: 1 }, { b: 1 }]
+
+function f() {
+  var a = 2
+  var c = 4
+
+  var [testb] = test
+  function g() {
+    console.log('-----')
+    console.log(a++) //2
+    console.log(b++) //3
+    console.log(c++) //6
+    console.log(testb)
+  }
+
+  setTimeout(() => {
+    test[0] = { c: 1 }
+    // test[0].a = 3
+  }, 0);
+
+  return g
+}
+
+console.log(a)//1
+console.log(b) // underfind
+console.log(c) // underfind 
+
+var g = f()
+var b = 3
+var c = 5
+
+g() // a:2 b:3 c:4 {b:1}
+setTimeout(() => {
+  // var g = f()
+  g()  // a:2 b:4 c:4 {a:3}
+}, 0)
+
+console.log('-----')
+console.log(a) //1
+console.log(b) //4
+console.log(c) //5
--- prototype.js
+++ prototype.js
@@ -3,21 +3,46 @@
  * @Author: 芦杰
  * @Description: 研究原型专用
  * @LastEditors: 芦杰
- * @LastEditTime: 2020-07-07 10:08:53
+ * @LastEditTime: 2020-07-07 10:13:45
  */
 
-const Tree = function () { }
+// const Tree = function () { }
 
-Tree.prototype.oneMoreLeaf = function () {
-  this.leaf = this.leaf + 1
-  console.log(`I have ${this.leaf} leafs`)
+// Tree.prototype.oneMoreLeaf = function () {
+//   this.leaf = this.leaf + 1
+//   console.log(`I have ${this.leaf} leafs`)
+// }
+
+// Tree.prototype.leaf = 0
+
+// const tree1 = new Tree()
+// tree1.oneMoreLeaf()
+// tree1.oneMoreLeaf()
+
+// const tree2 = new Tree()
+// tree2.oneMoreLeaf()
+
+/**
+  * 研究原型专用
+  */
+function Test() {
+  // this.a = 3
 }
 
-Tree.prototype.leaf = 0
+Test.prototype.changeA = function () {
+  this.a = this.a++
+  this.constructor.prototype.a = 3
+}
 
-const tree1 = new Tree()
-tree1.oneMoreLeaf()
-tree1.oneMoreLeaf()
+Test.prototype.a = 2
 
-const tree2 = new Tree()
-tree2.oneMoreLeaf()
+const test = new Test()
+console.log(test.a)
+test.changeA()
+console.log(test)
+
+const test2 = new Test()
+
+console.log(test2.a)
+test2.changeA()
+console.log(test2)  
